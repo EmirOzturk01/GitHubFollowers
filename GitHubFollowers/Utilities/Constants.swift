92e4a4db498d2b656061284db57433e9fae5f16e
EmirOzturk01 <ozturkemir2001@gmail.com>
2024-02-04 03:53:15 +0300

    Child ViewController - UserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Emir Öztürk on 4.02.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
